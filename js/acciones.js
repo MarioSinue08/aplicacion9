83ba7966108497a8f233664f23c9350bcfe00d62
MarioSinue08 <mario.dsaupoo3e@gmail.com>
2016-05-12 08:55:05 -0500

    3subida

--- js/acciones.js
+++ js/acciones.js
@@ -4,25 +4,25 @@ $(document).ready(function(e) {
     document.addEventListener("deviceready",function(){
 		
 		$('#izquierda').on("swipeleft",function(){
-			navigator.notification. alert("Deslizo a la izquierda",function(){"Aplicacion8","Aceptar"})
+			navigator.notification.alert("Deslizo hacia la izquierda",function(){"Aplicación 8","Aceptar"});
 });
 
 $('#derecha').on("swiperight",function(){
-	navigator.notification. confirm("¿Que quieres hacer?",function(opt){
+	navigator.notification.confirm("¿Que quieres hacer?",function(opt){
 		switch(opt)
 		{
 			case 1:
 			navigator.notification.beep(1);
 			break;
 			
-			case 1:
-			navigator.notification.vibrate (1000);
+			case 2:
+			navigator.notification.vibrate(1000);
 			break;
 		}
-	},"Aplicacion8","Beep,Vibrar,Cancelar");
-		
+	},"Aplicación 8","Beep,Vibrar,Cancelar");
+	
 });
-},false);
+	},false);
 });
 $(document).ready(function(e){
     document.addEventListener("deviceready",function(){
